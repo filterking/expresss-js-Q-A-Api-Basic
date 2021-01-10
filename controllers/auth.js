874f6234c6cea16ebe59d24fb9a96982d9204de2
Kunter kutlu <kunterkutlu@gmail.com>
2021-01-10 18:56:31 +0300

    user modeli oluşturuldu ve auth register controllerinda mongo veritabanına kullanıcı girişi yapıldı

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,19 @@
+const User=require("../models/User");
+
 const auth=(req,res,next)=>{
     res.status(200).json({success:true,mesaj:"auth controller ok"});
 };
 
-const register = (req,res,next)=>{
-    res.status(200).json({success:true,mesaj:"register ok"});
+const register = async (req,res,next)=>{
+    const name="Mustafa Kunter Kutlu";
+    const email="kunter.kutlu@goldstore.com.tr";
+    const password="1234567";
+    const user=await User.create({
+        name:name,
+        email:email,
+        password:password
+    });
+    res.status(200).json({success:true,data:user});
 };
 
 module.exports={auth,register};
---
+++ models/User.js
@@ -0,0 +1,55 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+const UserSchema=new Schema({
+    name:{
+        type:String,
+        required:[true,"Please Prpvide a name"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please provide an email address"],
+        unique:[true,"This email address hass been taken before"],
+        match:[
+            /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+            "Please provide a valid email address"
+        ]
+    },
+    role:{
+        type:String,
+        default:"user",
+        enum:["user","admin"]
+    },
+    password:{
+        type:String,
+        required:[true,"Please provide a password"],
+        minlength:[6,"please provide a password with min length 6"],
+        select:false
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+    title:{
+        type:String
+    },
+    about:{
+        type:String
+    },
+    place:{
+        type:String
+    },
+    website:{
+        type:String
+    },
+    rofile_image:{
+        type:String,
+        default:"default.jpg"
+    },
+    blocked:{
+        type:Boolean,
+        default:false
+    }
+
+});
+
+module.exports=mongoose.model("User",UserSchema);
